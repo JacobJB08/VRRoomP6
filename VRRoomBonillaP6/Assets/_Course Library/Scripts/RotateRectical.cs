30e21e9c7522f29a64cbe7d32af73b2f6cd5eaed
JacobJB08 <116194207+JacobJB08@users.noreply.github.com>
2023-10-16 13:48:16 -0700

    fire

---
+++ VRRoomBonillaP6/Assets/_Course Library/Scripts/RotateRectical.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateRectical : MonoBehaviour
+{
+    public GameObject self;
+    public float spinSpeed;
+
+    private void Start()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+}
